8406bac8bc9ed09a9a0ea656235755acd82f10ec
Caitlin Ross <caitlin.ross@kitware.com>
2021-11-09 11:56:52 -0500

    adding fides support

--- Tutorial/gs-inline-catalyst/simulation/reader.h
+++ Tutorial/gs-inline-catalyst/simulation/reader.h
@@ -7,6 +7,12 @@
 #include "gray-scott.h"
 #include "settings.h"
 
+#ifdef USE_FIDES
+#include <fides/DataSetReader.h>
+#endif
+
+#include <memory>
+
 class Reader
 {
 public:
@@ -21,10 +27,15 @@ protected:
     const Settings &settings;
 
     adios2::IO io;
+#if USE_FIDES
+    std::unique_ptr<fides::io::DataSetReader> FidesReader;
+    std::unordered_map<std::string, std::string> FidesPaths;
+#else
     adios2::Engine reader;
     adios2::Variable<double> var_u;
     adios2::Variable<double> var_v;
     adios2::Variable<int> var_step;
+#endif
 };
 
 #endif
